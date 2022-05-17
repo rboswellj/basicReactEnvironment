ecc2dcbe56af3de84fec27f96034141419154389
rboswellj <rboswellj@gmail.com>
2022-05-17 14:17:53 -0400

    Integrated CSS into webpack

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from "react-dom/client";
 window.React = React;
 import App from "./components/app";
+import "./styles/main.css";
 
 const container = document.getElementById("mountNode");
 ReactDOM.hydrateRoot(container, <App />);
--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,23 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.js$|.jsx$/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
         },
       },
-    ],
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
   },
+
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: ['', '.js', '.jsx', '.css']
  },
 };
