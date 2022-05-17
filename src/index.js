2cc765cb5e439dfca69b2265791765ace54269a9
rboswellj <rboswellj@gmail.com>
2022-05-17 13:40:26 -0400

    environment running test app successfully

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import ReactDOM from "react-dom";
-
+import React from 'react';
+import ReactDOM from "react-dom/client";
+window.React = React;
 import App from "./components/app";
 
-const container = document.getElementById("root");
+const container = document.getElementById("mountNode");
 ReactDOM.hydrateRoot(container, <App />);
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,16 @@
 module.exports = {
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader",
-          },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
         },
-      ],
-    },
-  };
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+ },
+};
